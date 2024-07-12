4419bb40d0527f33a1c2507f5d8690b7d536bc98
MoreThanGame <antonpleshakov.job@gmail.com>
2024-07-12 13:48:52 +0300

    final4

--- src/Engine.php
+++ src/Engine.php
@@ -5,24 +5,6 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function askQuestion($question, $rightChoice)
-{
-    line("Question: $question");
-    $userChoice = prompt("Your answer: ");
-    return (string) $userChoice === $rightChoice;
-}
-
-function compareAnswers()
-{
-    if ($userChoice === $rightChoice) {
-        line("Correct!");
-        $correctAnswers++;
-    } else {
-        line("'$userChoice' is wrong answer ;(. Correct answer was '$rightChoice'.\nLet's try again, $name!");
-        $i = 3;
-    }
-}
-
 function endGame($correctAnswers, $name)
 {
     if ($correctAnswers === 3) {
